0a640a765ceeaa3d0d0007c8afdb8d27977f2377
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2022-04-17 23:34:44 +0500

    Update Lite.py

--- Lite.py
+++ Lite.py
@@ -5,9 +5,13 @@ except:
    os.system('pip2 install requests')
 from time import sleep
 import requests
+def XXX(xxx):
+	for xx in xxx + "\n":
+		sys.stdout.write(xx);sys.stdout.flush();time.sleep(0.03)
 bit = platform.architecture()[0]
 if bit == '64bit':
     from li import _site_view_
+    XXX("\n \033[38;2;255;127;0;1mSystem Loading _________033[1;0m\n")
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     _site_view_()
@@ -16,4 +20,5 @@ elif bit == '32bit':
     print("\n Congratulations! Your device supported!\n")
     time.sleep(3)
     _site_view_()
+ 
  
